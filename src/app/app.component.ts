bffaa47da703becff90653dc5ba34893e20f2372
Dalbert Ang <da@architech.ca>
2025-02-28 16:31:20 -0500

    feat: add websocket connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatService } from './services/ChatService';
+import { FormsModule } from '@angular/forms';
+import { NgForOf } from '@angular/common';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'live-chat-frontend';
+
+  messages: string[] = [];
+  messageInput = '';
+
+  constructor(private chatService: ChatService) {
+    this.chatService.getMessages()
+    .pipe(
+        tap(msg => console.log('Received message: ', msg))
+    )
+    .subscribe(msg => this.messages.push(msg));
+  }
+
+  sendMessage() {
+    if (this.messageInput.trim()) {
+      console.log('*** this.messageInput: ', this.messageInput);
+      this.chatService.sendMessage(this.messageInput);
+      this.messageInput = '';
+    }
+  }
 }
